3436e94c4a1aa2b8220ca9e2c30c6bd4b7344797
JoshGrant5 <joshgg@icloud.com>
2020-12-24 13:26:49 -0800

    Use ngFor to display all recipes in recipes array

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'A test recipe', 'https://www.pexels.com/photo/cooked-food-704569/')
+  ];
 
   constructor() { }
 
