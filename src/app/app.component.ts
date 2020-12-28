a242a52bab2ce508e3dda21127649ba2e84ddd7f
JoshGrant5 <joshgg@icloud.com>
2020-12-28 15:50:03 -0800

    Add nav functionality with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+    console.log(feature)
+  }
 }
