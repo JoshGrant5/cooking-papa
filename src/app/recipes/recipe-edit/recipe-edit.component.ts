a82c57944aea0876af62582e17acde90487b2ad6
JoshGrant5 <joshgg@icloud.com>
2020-12-30 15:42:08 -0800

    Prepopulate the form with selected recipe data for editing

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,10 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
 
   id: number;
-
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -20,8 +22,28 @@ export class RecipeEditComponent implements OnInit {
          // "+" converts into a number
         this.id = +params['id'];
         this.editMode = params['id'] !== null;
+        this.initForm;
       }
     )
+  }
+
+  // Prepopulate the form with selected recipe data for editing
+  private initForm() {
+    let recipeName = '';
+    let imagePath = '';
+    let description = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(description)
+    });
   }
 
 }
