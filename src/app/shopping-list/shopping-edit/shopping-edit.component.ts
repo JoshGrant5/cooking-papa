8c12a0488d0f52b5737fd7e0d4e31adbf43b7c12
JoshGrant5 <joshgg@icloud.com>
2020-12-30 15:07:06 -0800

    Populate the form with the clicked item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
@@ -10,10 +10,12 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('form', {static: false}) slForm: NgForm;
 
   subscription: Subscription;
   editMode = false;
   editedIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -22,6 +24,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.editMode = true;
         this.editedIndex = index;
+        this.editedItem = this.slService.getIngredient(index);
+        // Whenever we select a new item, populate the form with the right values
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
       }
     )
   }
