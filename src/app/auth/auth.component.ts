76f329ba68aad9d0ec1feef875d286a492fe13a5
JoshGrant5 <joshgg@icloud.com>
2020-12-31 15:05:18 -0800

    Add email and password requirements

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  loginMode: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSwitchMode() {
+    this.loginMode = !this.loginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
   }
 
 }
