4ca5171856b7427fc71b73823e4255c12e9442eb
JoshGrant5 <joshgg@icloud.com>
2020-12-31 11:03:05 -0800

    Create put request to firebase database

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../shared/database.service';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  collapsed= true;
+  collapsed = true;
 
-  constructor() { }
+  constructor(private databaseService: DatabaseService) { }
 
   ngOnInit(): void {
+  }
+
+  onSaveData() {
+    this.databaseService.storeRecipes();
   }
 
 }
--- src/app/shared/database.service.ts
+++ src/app/shared/database.service.ts
@@ -1,7 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
 export class DatabaseService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://cooking-papa-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
