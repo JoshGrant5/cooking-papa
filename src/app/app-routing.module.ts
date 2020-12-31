a521d69fa2b2d1dd299b4e4d39b8a6dee5605555
JoshGrant5 <joshgg@icloud.com>
2020-12-31 14:56:16 -0800

    Add authentication component with login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 // resolvers added to certain routes => will run resolver code before the route is loaded
 const routes: Routes = [
@@ -27,7 +28,8 @@ const routes: Routes = [
       }
     ]
   },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'authentication', component: AuthComponent}
 ];
 
 @NgModule({
