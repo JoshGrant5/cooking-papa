f6adce9a08ce9b3da7b67d555e206d396733766e
JoshGrant5 <joshgg@icloud.com>
2020-12-31 18:24:11 -0800

    Login or signup redirects to recipes page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService, AuthResponseData } from './auth.service';
 
@@ -14,7 +15,7 @@ export class AuthComponent implements OnInit {
   loading: boolean = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -39,6 +40,7 @@ export class AuthComponent implements OnInit {
     authObservable.subscribe(response => {
       console.log(response);
       this.loading = false;
+      // this.router.navigate(['/recipes']);
     },
       errorMessage => {
         this.error = errorMessage;
