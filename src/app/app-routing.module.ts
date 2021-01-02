0d4fa123530267853317bf72f0659e3d74bdbf65
JoshGrant5 <joshgg@icloud.com>
2021-01-01 16:40:53 -0800

    Implement lazy loading to decrease file size

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthComponent } from './auth/auth.component';
-
 // resolvers added to certain routes => will run resolver code before the route is loaded
 const routes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  // Only load the module pointed to when the user visits the 'recipes' path (lazy loading) => loadChildren takes an inline import function, which returns a promise returning the module you may access
+  { path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module')
+    .then(module => module.RecipesModule)
+  }
 ];
 
 @NgModule({
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+
 import { SharedModule } from "../shared/shared.module";
+
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
