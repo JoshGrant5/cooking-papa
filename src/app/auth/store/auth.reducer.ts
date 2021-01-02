c15841a030c24e19091d7d30b04c4942dcb881e5
JoshGrant5 <joshgg@icloud.com>
2021-01-02 13:21:48 -0800

    Add login and logout cases to auth reducer

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,5 +1,5 @@
-
 import { User } from "../user.model";
+import * as AuthActions from './auth.actions';
 
 export interface AuthState {
   user: User;
@@ -9,6 +9,16 @@ const initialState: AuthState = {
   user: null
 };
 
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AllActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+    const user = new User(action.payload.email, action.payload.userId, action.payload.token, action.payload.expirationDate);
+    return {...state, user: user};
+
+    case AuthActions.LOGOUT:
+      return {...state, user: null};
+
+    default:
+      return state;
+  }
 }
